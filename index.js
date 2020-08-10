b771ca2f46d5d003b42051847397ea08c726e00e
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-08-10 10:12:40 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books').then(function(response) {
+    return response.json();
+  }).then(function(json){
+    renderBooks(json);
+  });
 }
 
 function renderBooks(books) {
@@ -12,5 +16,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+  fetchBooks();
+});
+
